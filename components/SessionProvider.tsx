712be8a02727f2d2b3b34f66758bb1678c12714c
Edvusae <105985332+Edvusae@users.noreply.github.com>
2025-12-23 07:37:07 +0200

    feat: Clear comment for the sessionProvider.tsx

--- components/SessionProvider.tsx
+++ components/SessionProvider.tsx
@@ -2,10 +2,15 @@
 
 import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
 
+// components/SessionProvider.tsx
 export default function SessionProvider({
   children,
 }: {
   children: React.ReactNode;
-}) {
-  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+}) 
+// components/SessionProvider.tsx
+{
+  return <NextAuthSessionProvider>
+    {children}
+    </NextAuthSessionProvider>;
 }
